e0fc6ce71f363aafd5dc83c2c0b6758e2bae5df5
Rahul Nayak <pragyes31@users.noreply.github.com>
2018-12-13 11:43:37 +0530

    Click handler for choosing weapon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.css";
+
+function createTicTacToeGame() {
+  const weaponButtons = document.querySelectorAll(".weapon");
+  // console.log(weaponButtons);
+  let userWeapon = "";
+  let botWeapon = "";
+  const ticTacToe = {
+    chooseWeapon: e => {
+      userWeapon = e.target.innerHTML;
+      botWeapon = userWeapon === "X" ? "O" : "X";
+      console.log({ userWeapon, botWeapon });
+      weaponButtons.forEach(e => (e.disabled = true));
+    }
+  };
+  weaponButtons.forEach(e =>
+    e.addEventListener("click", ticTacToe.chooseWeapon)
+  );
+}
+
+const ticTacToe = createTicTacToeGame();
